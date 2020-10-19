f827710094d3452948477abc4865569f61afac1a
Glenn Beall <glenn@beall.id.au>
2020-10-19 19:23:09 +0800

    Add table header exceptions to validator

--- validate.py
+++ validate.py
@@ -5,7 +5,12 @@ def validateHTMLStyleAttribute(testCase):
     if re.search("^((\w+(-\w+)*:\w+);)+$", testCase) is None:
         raise exceptions.invalidStyleParameter(f'{testCase} - Invalid style attribute given. Format should contain at least one set of "word:word;"')
 
-
 def validateHTMLClassAttribute(testCase):
     if re.search("^[_\-a-zA-Z]{1}\w+$", testCase) is None:
         raise exceptions.invalidClassParameter(f'{testCase} - Invalid class attribute given. Format should contain at least one set of "word"')
+
+def validateHTMLTableHeader(testCase, data):
+    if testCase == '':
+        raise noTableHeaderException(f'The table must contain a header row.')
+    if leng(testCase) != len(data):
+        raise tableHeaderDataSizeMismatchException(f'Different number of columns between the header and data')
